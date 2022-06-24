7ab378cb2a188f29d20b304af6be904052b0f817
1sh1vam <shivam07.mt@gmail.com>
2022-06-24 18:37:21 +0530

    modularizing the form to add goal

---
+++ components/GoalInput.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+
+const GoalInput = ({ addGoal }) => {
+  const [text, setText] = useState('');
+
+  const handleGoalInp = (enteredText) => {
+    setText(enteredText);
+  };
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        onChangeText={handleGoalInp}
+        value={text}
+        placeholder="Your course goal!"
+        style={styles.textInput}
+      />
+      <Button onPress={() => addGoal(text)} title="Add Goal" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomColor: '#c4c4c4',
+    borderBottomWidth: 1,
+    marginBottom: 24,
+  },
+  textInput: {
+    width: '70%',
+    borderWidth: 1,
+    borderColor: '#c4c4c4',
+    padding: 8,
+    marginRight: 8,
+  },
+});
+
+export default GoalInput;
