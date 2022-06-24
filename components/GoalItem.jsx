e3b9dac52995bdda81114252c72e32b75db7b87d
1sh1vam <shivam07.mt@gmail.com>
2022-06-24 20:07:29 +0530

    adding the glitter effect on item

--- components/GoalItem.jsx
+++ components/GoalItem.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Pressable } from 'react-native'
 
 const GoalItem = ({ text }) => {
   return (
     <View style={styles.goalItem}>
+    <Pressable style={({ pressed }) => pressed && styles.pressable}>
         <Text style={styles.goalText}>{text}</Text>
+    </Pressable>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
     goalItem: {
-        padding: 15,
         borderRadius: 10,
         backgroundColor: '#2D5C54',
         margin: 5,
       },
       goalText: {
+        padding: 15,
         color: '#fff',
       },
+      pressable: {
+        backgroundColor: '#3ae056',
+        opacity: 0.5,
+      }
 })
 
 export default GoalItem
